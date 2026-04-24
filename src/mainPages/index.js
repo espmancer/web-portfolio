641ada52555bf4ca68ca11351d5e94326b4a23e4
Selisine <Aidenpike113@gmail.com>
2026-04-24 16:07:20 +0000

    convert dictionary mapping to lists

--- src/mainPages/index.js
+++ src/mainPages/index.js
@@ -1,26 +1,31 @@
 // The best solution I can think of that doesn't involve some weird File I/O workaround.
 const dictionaries = new Map();
 
-dictionaries.set('ictukV5', "o|no,not,zero|O|No, Not, Zero|Adv.,Adj.,Adj.,Intj.,N.|used as a function word to make negative a group of words or a word,not any,having no magnitude or quantity,not so,an act or instance of refusing or denying by the use of the word no||o\nkofa|be|Kofa|Be|V.,V.|to have identity with: to constitute the same idea or object as,to have a specified qualification or characterization||kofa")
-dictionaries.set('ictukV6', "o|no,not,zero|O|No, Not, Zero|Adv.,Adj.,Adj.,Intj.,N.|used as a function word to make negative a group of words or a word,not any,having no magnitude or quantity,not so,an act or instance of refusing or denying by the use of the word no|From Kzhkaa \"kka\",\"not\" or later just \"ka\", mishearing of \"kk\", \"not\" from Kzh.|o\nkofa|be|Kofa|Be|V.,V.|to have identity with: to constitute the same idea or object as,to have a specified qualification or characterization|From Zhoktuk \"gofa\", \"being, existence\", from Kzhokuut \"goa\", \"living thing\", from Kzhkaa \"ka\", \"living person\", from Kzh \"k\", \"person\"|kofa");
+dictionaries.set('ictukV5', [
+  "o|no,not,zero|O|No, Not, Zero|Adv.,Adj.,Adj.,Intj.,N.|used as a function word to make negative a group of words or a word,not any,having no magnitude or quantity,not so,an act or instance of refusing or denying by the use of the word no|",
+  "kofa|be|Kofa|Be|V.,V.|to have identity with: to constitute the same idea or object as,to have a specified qualification or characterization|"
+]);
+dictionaries.set('ictukV6', [
+  "o|no,not,zero|O|No, Not, Zero|Adv.,Adj.,Adj.,Intj.,N.|used as a function word to make negative a group of words or a word,not any,having no magnitude or quantity,not so,an act or instance of refusing or denying by the use of the word no|From Kzhkaa \"kka\",\"not\" or later just \"ka\", mishearing of \"kk\", \"not\" from Kzh.",
+  "kofa|be|Kofa|Be|V.,V.|to have identity with: to constitute the same idea or object as,to have a specified qualification or characterization|From Zhoktuk \"gofa\", \"being, existence\", from Kzhokuut \"goa\", \"living thing\", from Kzhkaa \"ka\", \"living person\", from Kzh \"k\", \"person\""
+]);
 
 const conlangEntries = new Map();
 const englishEntries = new Map();
 
 function buildEntryMaps(dictionaryKey){
-  rawEntries = dictionaries.get(dictionaryKey).split('\n')
+  rawEntries = dictionaries.get(dictionaryKey)
   
   for (let i = 0; i < rawEntries.length; i++){
     rawEntry = rawEntries[i].split("|");
     key = rawEntry[0];
+    conlangEntries.set(key, rawEntries[i])
     englishKeys = rawEntry[1].split(',');
-    value = rawEntry.slice(2).join('|');
-    conlangEntries.set(key, value)
     
     // Set English keys
     for (let x = 0; x < englishKeys.length; x++){
       key = englishKeys[x]
-      englishEntries.set(key, value)
+      englishEntries.set(key, rawEntries[i])
     }
   }
 }
@@ -28,20 +33,27 @@ function buildEntryMaps(dictionaryKey){
 //Helper function to build the entry being displayed
 function buildEntry(rawEntry, dictionaryKey){
   let elements = rawEntry.split('|');
-  let name = elements[0];
-  let translation = elements[1];
-  let types = elements[2].split(',');
-  let definitions = elements[3].split(',');
-  let etymology = elements[4];
-  let image = elements[5];
+  let key = elements[0];
+  let name = elements[2];
+  let translation = elements[3];
+  let types = elements[4].split(',');
+  let definitions = elements[5].split(',');
+  let etymology = elements[6];
   let entry = `<div>${name} | ${translation}<ol>`;
   
+  // Format definitions
   for (let i = 0; i < types.length; i++){
     entry += `<li><i>${types[i]}</i> ${definitions[i]}</li>`;
   }
 
+  // Add etymology
   entry += `</ol>${etymology}`;
-  entry += `<image src="../../../resources/${dictionaryKey}/${image}.svg"></image></div><hr class="headerSeperator"/>`;
+  // Add image
+  entry += `<image src="../../../resources/${dictionaryKey}/glyphs/${key}.svg"></image>`;
+  // Add audio
+  entry += `<audio controls><source src="../../../resources/${dictionaryKey}/sounds/${key}.ogg" type="audio/ogg">`;
+  entry += `<source src="../../../resources/${dictionaryKey}/sounds/${key}.mp3" type="audio/mp3">`;
+  entry += `Your browser does not support the audio tag.</audio></div><hr class="headerSeperator"/>`;
   
   return entry;
 }
