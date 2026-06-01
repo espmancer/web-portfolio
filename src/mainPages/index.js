9d9ad11aebb6f8a7f9d33fb8a1a8f8b3bb5adb67
Selisine <Aidenpike113@gmail.com>
2026-06-01 14:48:56 -0400

    add maybe

--- src/mainPages/index.js
+++ src/mainPages/index.js
@@ -317,7 +317,7 @@ class IctukV5 extends Dictionary {
     "upapa|less|Adj.|constituting a more limited number or amount|",
     "ushko|very|Adv.|to a high degree|",
     "shaa|than|Conj.,Prep.|used as a function word to indicate the second member or the member taken as the point of departure in a comparison expressive of inequality,in comparison with|",
-    "fashofash|somewhat,so-so,normal|Adv.,Adj.,Adj.|in some degree or measure,moderately well,conforming to a type/standard/regular pattern|",
+    "fashofash|somewhat,so-so,normal,maybe|Adv.,Adj.,Adj.,Adv.|in some degree or measure,moderately well,conforming to a type/standard/regular pattern,perhaps|",
     "ushkonguho|confident|Adj.|full of conviction",
     "oouktuzha|foreign,weak|Adj.,Adj.|born in/belonging to/characteristic of some place or country other than the one under consideration,physically/mentally/intellectually deficient|",
     "paazh|want,desire|V.,N.|to have or feel need,conscious impulse toward something that promises enjoyment or satisfaction in its attainment|",
