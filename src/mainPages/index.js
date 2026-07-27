c4c475780e233b818adc9fc5da5421b16b8ec6f0
Selisine <Aidenpike113@gmail.com>
2026-07-27 15:16:27 -0400

    add pipe

--- src/mainPages/index.js
+++ src/mainPages/index.js
@@ -522,7 +522,7 @@ class IctukV5 extends Dictionary {
     "zhupo|open|Adj.|being in a position or adjustment to permit passage|",
     "ofazhupo|close|V.|to move so as to bar passage through something|",
     "ozhupo|closed|Adj.|not open|",
-    "tufoa|name|N.|a word or phrase that constitutes the distinctive designation of a person or thing"
+    "tufoa|name|N.|a word or phrase that constitutes the distinctive designation of a person or thing|"
 
   ];
   invisible = ['/', '|', '<'];
